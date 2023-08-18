6fced71bb5cd8d03f2e2fa3ee84b2cef9f9fb78a
Olegreg762 <109924953+Olegreg762@users.noreply.github.com>
2023-08-17 19:39:57 -0500

    add static to and port listen message to server.js

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 const express = require("express");
 
 const app = express();
+const PORT = 3001;
 
+app.use(express.static("public"));
+
+app.listen(PORT, ()=>
+    console.log(`App is listening at http://localhost:${PORT}`)
+)

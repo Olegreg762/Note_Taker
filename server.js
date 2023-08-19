26afb51fb793e1fb73d1074bcc69bc835d932576
Olegreg762 <109924953+Olegreg762@users.noreply.github.com>
2023-08-19 05:22:48 -0500

    begin api_routes.js

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require("express");
-const html_router = require("./routes/html_routes");
-const api_router = require("./routes/api_routes");
+const html_route = require("./routes/html_routes");
+const api_route = require("./routes/api_routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use(html_router);
-app.use(api_router);
+app.use(html_route);
+app.use(api_route);
 
 
 app.listen(PORT, ()=>

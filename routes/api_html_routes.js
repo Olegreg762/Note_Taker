5afe672a4a4058a5494235a428e7ba42c11ac945
Olegreg762 <109924953+Olegreg762@users.noreply.github.com>
2023-08-25 12:22:00 -0500

    add comments

--- routes/api_html_routes.js
+++ routes/api_html_routes.js
@@ -1,18 +1,22 @@
+// Module imports
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const {v4 : uuidv4} = require("uuid");
+// Initailize express router 
 const router = express.Router();
-
+// Path for json used as a database
 const notes_path = "../db/db.json";
+// Sets the database file to be read as JSON
 let notes_db = JSON.parse(fs.readFileSync(path.join(__dirname, notes_path)))
 
 ////// ## API Routing ## ////// 
 
+// Sets the route for notes takinn data
 router.get("/api/notes",(req,res)=>{
     res.json(notes_db)
 });
-
+// Sets route for how a new notes will be processed
 router.post("/api/notes",(req,res)=>{
     const new_note = {
         title: req.body.title,
@@ -23,7 +27,7 @@ router.post("/api/notes",(req,res)=>{
     fs.writeFileSync(path.join(__dirname, notes_path),JSON.stringify(notes_db));
     res.json(new_note);
 });
-
+// Determines how note deletes will be processed
 router.delete("/api/notes/:id", (req,res)=>{
     const note_id = req.params.id;
     notes_db = notes_db.filter((note)=> note.id !== note_id);
@@ -33,13 +37,14 @@ router.delete("/api/notes/:id", (req,res)=>{
 
 ////// ## HTML Routing ## ////// 
 
+// route for the HTML file for notes  page
 router.get("/notes",(req,res)=>{
     res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 
-
+// Sets route for all undefined routes to route to main landing page index.html
 router.get("*",(req,res)=>{
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
-
+// Exports the router instant
 module.exports = router;
--- server.js
+++ server.js
@@ -1,14 +1,17 @@
+// Module imports
 const express = require("express");
 const api_html_route = require("./routes/api_html_routes");
+// Initailize express as app
 const app = express();
+// Set PORT variable that will change based on if the app is deployed via a service or locally
 const PORT = process.env.PORT || 3002;
-
+// Functions for express app to use
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use("/",api_html_route);
 
-
+// Port that express app is listening to
 app.listen(PORT, ()=>
     console.log(`App is listening at http://localhost:${PORT}`)
 );
